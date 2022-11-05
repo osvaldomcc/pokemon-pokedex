bbf9121ee40bae34b690a1f9b5b1f2b79f0831ea
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-11-05 17:38:34 -0400

    Add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      blueLigth: '#8074A7',
+      blueDark: '#312769',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
