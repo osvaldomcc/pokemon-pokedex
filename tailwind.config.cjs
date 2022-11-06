f513d12883d5a773cae8eeb4506a12ff90f649d5
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-11-06 01:51:14 -0500

    Fix the colors config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      blueLigth: '#8074A7',
-      blueDark: '#312769',
+    extend: {
+      colors: {
+        blueLigth: '#8074A7',
+        blueDark: '#312769',
+        lightGray: '#EDEEF3',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
