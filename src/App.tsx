fc82549956147fad4c5679c887f5f5a9ba015c29
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-11-08 15:19:23 -0500

    Setup the entry point of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
+
+import { router } from '@/router'
+import { MyApp } from '@/components/commons'
+
+const queryClient = new QueryClient()
+
 const App = () => {
-  return <div>Remove me and Start Coding</div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <MyApp>
+          <RouterProvider router={router} />
+        </MyApp>
+      </HelmetProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
