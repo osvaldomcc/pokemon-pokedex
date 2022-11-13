4e6b88bbb647e2fe2ea53a4ebc989a4e6f19a6d5
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-11-12 23:40:27 -0500

    Add data test loading

--- src/components/commons/Loader.tsx
+++ src/components/commons/Loader.tsx
@@ -2,7 +2,10 @@ import { ThunderIcon } from '@/components/icons'
 
 export const Loader = () => {
   return (
-    <div className="absolute backdrop-blur-md inset-0 z-20">
+    <div
+      className="absolute backdrop-blur-md inset-0 z-20"
+      data-testid="loading"
+    >
       <div className="flex h-screen justify-center items-center dark:text-gray-300 text-blueDark">
         <ThunderIcon className="w-9 h-9 animate-bounce" />
       </div>
