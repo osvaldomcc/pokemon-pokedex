6bb8ec04396ddd4645dc5a60d3843e4f9422f4ea
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2022-11-18 21:26:39 -0500

    Update the method to fetch data

--- src/hooks/pokemon/usePokemons.tsx
+++ src/hooks/pokemon/usePokemons.tsx
@@ -1,77 +1,14 @@
+import { getAllPokemons } from '@/api'
 import { useInfiniteQuery } from '@tanstack/react-query'
-
-import { IInfiniteQuery } from '@/types'
-import { PokemonService } from '@/services'
-import { Pokemon } from '@/types/pokemon'
-import { imgPokemon } from '@/helpers'
 
 interface IProps {
   limit: number
 }
 
-export const getPokemonWithAbility = async (
-  pokemon: string
-): Promise<Pokemon> => {
-  const { data } = await PokemonService.getPokemon(pokemon)
-  const newPokemon: Pokemon = {
-    id: data.id,
-    base_experience: data.base_experience,
-    forms: data.forms,
-    name: data.name,
-    order: data.order,
-    species: data.species,
-    sprites: data.sprites,
-    stats: data.stats,
-    types: data.types,
-    weight: data.weight,
-    image: imgPokemon(data.id),
-    ability: '',
-  }
-  const ability = data.abilities
-  if (ability && ability[0].ability.name !== '') {
-    const id = ability[0].ability.url.match(/\d+/g)
-    if (id) {
-      const pokeAbility = await PokemonService.getAbilities(
-        id.length > 0 ? Number(id[1]) : 1
-      )
-      pokeAbility.data.effect_entries.forEach((entry) => {
-        if (entry.language.name === 'en') {
-          newPokemon.ability = entry.effect
-        }
-      })
-    }
-  }
-  return newPokemon
-}
-
-const getAllPokemons = async ({
-  pageParam = 0,
-  queryKey,
-}: IInfiniteQuery<IProps>) => {
-  const [, args] = queryKey
-
-  const { limit } = args as IProps
-
-  let pokemons: Pokemon[] = []
-  const pokemonsPromise: Promise<Pokemon>[] = []
-
-  const res = await PokemonService.getAllPokemons(pageParam * 10, limit)
-
-  if (res?.data?.results.length === 0) return pokemons
-
-  res.data.results.forEach((pokemon) => {
-    pokemonsPromise.push(getPokemonWithAbility(pokemon.name))
-  })
-
-  pokemons = await Promise.all(pokemonsPromise)
-
-  return pokemons
-}
-
 export const usePokemons = ({ limit = 10 }: IProps) => {
   const allPokemonsQuery = useInfiniteQuery({
     queryKey: ['pokemons', { limit }],
-    queryFn: getAllPokemons,
+    queryFn: getAllPokemons<IProps>,
     getNextPageParam: (lastPage, pages) => {
       if (lastPage.length === 0) return
       return Math.max(pages.length - 1, 0) + 1
